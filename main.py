3d781ca1982892e7b274d615d72791e0d84263fd
Muhammad Zeerak Khan <zeerak1994@outlook.com>
2023-10-15 21:10:04 +0200

    main.py tested as entry point till stage_02

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
-from src.Kidney_Tumor_Classification import logger
+from Kidney_Tumor_Classification import logger
+from Kidney_Tumor_Classification.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from Kidney_Tumor_Classification.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
-logger.info("This is the custom log")
+STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f">>>>> {STAGE_NAME} started <<<<<") 
+    data_ingestion_train_pipeline = DataIngestionTrainingPipeline()
+    data_ingestion_train_pipeline.main()       
+    logger.info(f">>>>> {STAGE_NAME} completed!<<<<<\n")
+
+except Exception as e:
+    logger.exception(e)
+    raise e    
+    
+STAGE_NAME = "Prepare Base Model Stage"
+
+try:
+    logger.info(f"*"*20)
+    logger.info(f">>>>> {STAGE_NAME} started <<<<<") 
+    prepare_base_model_train_pipeline = PrepareBaseModelTrainingPipeline()
+    prepare_base_model_train_pipeline.main()
+    logger.info(f">>>>> {STAGE_NAME} completed!<<<<<\n")
+
+except Exception as e:
+    logger.exception(e)
+    raise e        
