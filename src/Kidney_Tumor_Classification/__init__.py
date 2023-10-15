f83e87df5a75401e2230c4d0e9160651f24af5c2
Muhammad Zeerak Khan <zeerak1994@outlook.com>
2023-10-15 16:24:02 +0200

    logging string modified

--- src/Kidney_Tumor_Classification/__init__.py
+++ src/Kidney_Tumor_Classification/__init__.py
@@ -2,7 +2,7 @@ import os
 import sys 
 import logging
 
-logging_str = "%(asctime)s : %(levelname)s : %(module)s :%(filename)s : %(lineno)d: %(message)s"
+logging_str = "%(asctime)s : %(levelname)s :%(filename)s : %(lineno)d: %(message)s"
 
 log_dir = "logs"
 log_file_path = os.path.join(log_dir, "running_logs.log")
