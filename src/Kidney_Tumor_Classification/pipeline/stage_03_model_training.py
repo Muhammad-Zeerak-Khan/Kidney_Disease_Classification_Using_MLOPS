2470c251721ec264e3414866de595922039819ba
Muhammad Zeerak Khan <zeerak1994@outlook.com>
2023-10-20 19:21:20 +0200

    Stage_03 model training pipeline added

---
+++ src/Kidney_Tumor_Classification/pipeline/stage_03_model_training.py
@@ -0,0 +1,32 @@
+from Kidney_Tumor_Classification.config.configuration import ConfigurationManager
+from Kidney_Tumor_Classification.components.model_training import Training
+from Kidney_Tumor_Classification import logger
+
+STAGE_NAME = "Training"
+
+class ModelTrainingPipeline:
+    def __init__(self):
+        pass
+
+
+    def main(self):
+
+        config = ConfigurationManager()
+        training_config = config.get_training_config()
+        training = Training(config=training_config)
+        training.get_base_model()
+        training.train_valid_generator()
+        training.train()
+
+
+if __name__ == "__main__":
+    try:
+        logger.info(f"*"*20)
+        logger.info(f">>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = ModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>> stage {STAGE_NAME} completed <<<<\n\nx==============x")
+
+    except Exception as e:
+        logger.exception(e)
+        raise e    
