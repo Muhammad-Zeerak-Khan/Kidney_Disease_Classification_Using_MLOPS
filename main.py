bd12bc462b89a4d1bacd97aafa881a227a82b689
Muhammad Zeerak Khan <zeerak1994@outlook.com>
2023-10-20 19:25:28 +0200

    Checked till stage_03 training_pipeline

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from Kidney_Tumor_Classification import logger
 from Kidney_Tumor_Classification.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from Kidney_Tumor_Classification.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from Kidney_Tumor_Classification.pipeline.stage_03_model_training import ModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 try:
     logger.info(f">>>>> {STAGE_NAME} started <<<<<") 
     data_ingestion_train_pipeline = DataIngestionTrainingPipeline()
     data_ingestion_train_pipeline.main()       
-    logger.info(f">>>>> {STAGE_NAME} completed!<<<<<\n")
+    logger.info(f">>>>> {STAGE_NAME} completed!<<<<<\n\nx==========x")
 
 except Exception as e:
     logger.exception(e)
@@ -20,8 +21,20 @@ try:
     logger.info(f">>>>> {STAGE_NAME} started <<<<<") 
     prepare_base_model_train_pipeline = PrepareBaseModelTrainingPipeline()
     prepare_base_model_train_pipeline.main()
-    logger.info(f">>>>> {STAGE_NAME} completed!<<<<<\n")
+    logger.info(f">>>>> {STAGE_NAME} completed!<<<<<\n\nx==========x")
 
 except Exception as e:
     logger.exception(e)
-    raise e        
+    raise e  
+
+STAGE_NAME = "Training Stage"     
+try:
+    logger.info(f"*"*20)
+    logger.info(f">>>>> stage")
+    model_trainer = ModelTrainingPipeline()
+    model_trainer.main()
+    logger.info(f">>>>> {STAGE_NAME} completed!<<<<<\n\nx============x")
+
+except Exception as e:
+    logger.exception(e)    
+    raise e
